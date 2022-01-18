5e818acecf3ffd30b2dc5b12c14d5622f68a9361
Khaair <khair.cse.iu@gmail.com>
2022-01-18 08:08:57 +0600

    Update

---
+++ fp05.js
@@ -0,0 +1,19 @@
+const users = [
+    { name: 'John', age: 34 },
+    { name: 'Amy', age: 20 },
+    { name: 'camperCat', age: 10 }
+  ];
+  
+  const usersUnder30 = users.filter(user)
+
+  function user(ag){
+  return ag >=30
+  }
+  console.log(usersUnder30);
+
+//   const ages = [32, 33, 16, 40];
+// const result = ages.filter(checkAdult);
+
+// function checkAdult(age) {
+//   return age >= 18;
+// }
