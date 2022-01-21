2690d66265748535edd743ac136a9bb5e2f013b8
Khaair <khair.cse.iu@gmail.com>
2022-01-21 17:56:52 +0600

    Update

---
+++ fp20.js
@@ -0,0 +1,6 @@
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "h";
+let ourVariable = myArray[0];
+// Only change code above this line
+console.log(ourVariable);
